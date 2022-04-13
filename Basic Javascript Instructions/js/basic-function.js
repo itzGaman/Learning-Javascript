c8af2ac7e09c1f771428475e5250670be32521e9
itzGaman <itzgaman@gmail.com>
2022-04-13 17:40:45 +0200

    array added

---
+++ Basic Javascript Instructions/js/basic-function.js
@@ -0,0 +1,8 @@
+var msg = 'sign up to receive ....';
+
+function updateMessage(){
+    var el = document.getElementById('message');
+    el.textContent= msg;
+}
+
+updateMessage();
---
+++ Basic Javascript Instructions/js/geom.js
@@ -0,0 +1,7 @@
+
+function area(width,height){
+    return width*height;
+}
+var size = area(4,5);
+var el = document.getElementById('result');
+el.textContent= size;
