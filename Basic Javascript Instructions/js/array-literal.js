f9f696f882b8a74ffa89c21ba3cce757bc3f8d1f
itzGaman <itzgaman@gmail.com>
2022-04-13 17:18:35 +0200

    array added

---
+++ Basic Javascript Instructions/js/array-literal.js
@@ -0,0 +1,4 @@
+var colors;
+colors = ['white','black','custom'];
+var elColor = document.getElementById('color');
+elColor.textContent = colors[2];
--- Basic Javascript Instructions/js/numeric-variable.js
+++ Basic Javascript Instructions/js/numeric-variable.js
@@ -1,8 +1,10 @@
+// variabili intere
 var price;
 var quantity;
 var total;
 price = 5;
 quantity = 14;
 total = quantity * price;
-var el = document.getElementById('cost');
-el.textContent ='$' + total;
+// un oggetto
+var el = document.getElementById('cost'); // metodi
+el.textContent ='il totale : ' + total; // attributi
